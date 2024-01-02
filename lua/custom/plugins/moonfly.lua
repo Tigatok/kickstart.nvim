ea6d35c831eea92974dd0b16f280d67da2b10501
Tyler Marshall <tyler@dovetaildigital.ca>
2024-01-02 07:19:51 -0800

    fixes colorscheme

--- lua/custom/plugins/moonfly.lua
+++ lua/custom/plugins/moonfly.lua
@@ -1,7 +1,11 @@
 return {
-	"bluz71/vim-moonfly-colors",
-	name = "moonfly",
-	lazy = false, priority =  1001,
-
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme moonfly]])
+    end,
 }
 
