de12a023f3b89aa70b6c5dbffadf8e2f7b849818
Tyler Marshall <tyler@dovetaildigital.ca>
2024-01-02 06:44:55 -0800

    update

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,14 +1,47 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local nmap = function(keys, func, desc)
+  if desc then
+    desc = 'LSP: ' .. desc
+  end
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+  vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+end
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
 return {
-	"ThePrimeagen/harpoon",
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { {"nvim-lua/plenary.nvim"} },
+    config = function()
+        local harpoon = require("harpoon")
+        
+
+        harpoon:setup({})
+        -- basic telescope configuration
+        local conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+            }):find()
+        end
+        nmap("<C-e>", function() toggle_telescope(harpoon:list()) end,
+             "Open harpoon window")
+        nmap("<leader>a", function() harpoon:list():append() end)
+        nmap("<C-H>", function() harpoon:list():select(1) end)
+        nmap( "<C-t>", function() harpoon:list():select(2) end)
+        nmap( "<C-n>", function() harpoon:list():select(3) end)
+        nmap( "<C-S>", function() harpoon:list():select(4) end)
+        -- Toggle previous & next buffers stored within Harpoon list
+        nmap( "<C-S-P>", function() harpoon:list():prev() end)
+        nmap( "<C-S-N>", function() harpoon:list():next() end)
+    end
 }
 
