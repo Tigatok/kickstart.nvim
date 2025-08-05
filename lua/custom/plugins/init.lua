e6e4a96150e5a8365789f3b3b15bf23d5fb70eec
Tyler Marshall <tyler@dovetaildigital.ca>
2025-08-05 07:16:13 -0700

    update config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,23 +2,27 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-vim.wo.relativenumber = true
-vim.keymap.set('v', "J", ":m '>+1<CR>gv=gv");
-vim.keymap.set('v', "K", ":m '<-2<CR>gv=gv");
-vim.keymap.set('n', "<C-u>", "<C-u>zz");
-vim.keymap.set('n', "<C-d>", "<C-d>zz");
-vim.keymap.set('n', "n", "nzzzv");
-vim.keymap.set('n', "N", "Nzzzv");
-vim.keymap.set('n', "<leader>y", "\"+y");
-vim.keymap.set('v', "<leader>y", "\"+y");
-vim.keymap.set('n', "<leader>Y", "\"+y");
-vim.keymap.set("n", "<C-l>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>l", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-vim.keymap.set("n", "<leader>x", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set('n','<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({find_command = {'rg','--files','--hidden','--glob=!**/.git/*','--glob=!**/.idea/*','--glob=!**/.vscode/*','--glob=!**/build/*','--glob=!**/dist/*','--glob=!**/yarn.lock','--glob=!**/package-lock.json'}, })<cr>");
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex);
+vim.o.relativenumber = true
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+y')
+vim.keymap.set('n', '<C-l>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<leader>l', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+vim.keymap.set('n', '<leader>x', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set(
+  'n',
+  '<leader>ff',
+  "<cmd>lua require'telescope.builtin'.find_files({find_command = {'rg','--files','--hidden','--glob=!**/.git/*','--glob=!**/.idea/*','--glob=!**/.vscode/*','--glob=!**/build/*','--glob=!**/dist/*','--glob=!**/yarn.lock','--glob=!**/package-lock.json'}, })<cr>"
+)
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -26,120 +30,93 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.swapfile = false
 vim.o.scrolloff = 999
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 
+return {
+  {
+    'laytan/cloak.nvim',
 
-vim.filetype.add({
-  extension = {
-    mdx = 'tsx'
-  }
-})
-vim.treesitter.language.register('mdx', 'markdown')
-
-return{
-  {'laytan/cloak.nvim', 
-
-    config = function ()
-      require('cloak').setup({
-        enabled = true
-      })
-    end
+    config = function()
+      require('cloak').setup {
+        enabled = true,
+      }
+    end,
   },
   {
-    "github/copilot.vim",
-    config = function ()
-      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', {silent = true, expr= true});
-      vim.api.nvim_set_keymap("i", "<C-H>", 'copilot#Previous()', {silent = true, expr= true});
-      vim.api.nvim_set_keymap("i", "<C-K>", 'copilot#Next()', {silent = true, expr= true});
-    end
+    'github/copilot.vim',
+    config = function()
+      vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      vim.api.nvim_set_keymap('i', '<C-H>', 'copilot#Previous()', { silent = true, expr = true })
+      vim.api.nvim_set_keymap('i', '<C-K>', 'copilot#Next()', { silent = true, expr = true })
+    end,
   },
   {
-    "bluz71/vim-moonfly-colors",
-    name = "moonfly",
+    'bluz71/vim-moonfly-colors',
+    name = 'moonfly',
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme moonfly]])
-    end
+      vim.cmd [[colorscheme moonfly]]
+    end,
   },
-  --[[
-   {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {},
-    config = function ()
-
-      vim.keymap.set('n', '<leader>tt',function()
-        require('trouble').toggle()
-      end, {desc="Trouble Toggle"})
-
-      vim.keymap.set('n', '<leader>tn',function()
-        require('trouble').next({jump = true, skip_groups = true})
-        end, {desc="Trouble Next"})
-
-      vim.keymap.set('n', '<leader>tp',function ()
-        require('trouble').previous({jump = true, skip_groups = true})end,
-        {desc="Trouble Previous"})
-    end
-  },
-  -- ]]
   {
-  "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
-  cmd = "Trouble",
-  keys = {
-    {
-      "<leader>xx",
-      "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "Diagnostics (Trouble)",
-    },
-    {
-      "<leader>xX",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "Buffer Diagnostics (Trouble)",
-    },
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
       {
-        "<leader>tn",
-        "<cmd>lua require('trouble').next({skip_groups = true, jump = true})<cr>",
-        desc = "Next (Trouble)",
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
       },
-    {
-      "<leader>cs",
-      "<cmd>Trouble symbols toggle focus=false<cr>",
-      desc = "Symbols (Trouble)",
-    },
-    {
-      "<leader>cl",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP Definitions / references / ... (Trouble)",
-    },
-    {
-      "<leader>xL",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "Location List (Trouble)",
-    },
-    {
-      "<leader>xQ",
-      "<cmd>Trouble qflist toggle<cr>",
-      desc = "Quickfix List (Trouble)",
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>tn',
+        "<cmd>lua require('trouble').next({skip_groups = true, jump = true})<cr>",
+        desc = 'Next (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
     },
   },
-},
-  {
-    "nvim-telescope/telescope.nvim",
-    config = function ()
-      local telescope = require("telescope");
-      telescope.setup({
-        pickers = {
-          find_files = {
-            find_command = {
-              'rg',
-              '--files',
-              '--hidden',
-              '--glob',
-              '!**/.git/*',
-            }
-          }
-        }
-      })
-    end
-  }
+  --  {
+  --    'nvim-telescope/telescope.nvim',
+  --    config = function()
+  --      local telescope = require 'telescope'
+  --      telescope.setup {
+  --        pickers = {
+  --          find_files = {
+  --            find_command = {
+  --              'rg',
+  --              '--files',
+  --              '--hidden',
+  --              '--glob',
+  --              '!**/.git/*',
+  --            },
+  --          },
+  --        },
+  --      }
+  --    end,
+  --  },
 }
--- lua/custom/plugins/prettier.lua
+++ lua/custom/plugins/prettier.lua
@@ -1,9 +1,9 @@
-vim.cmd([[ let g:prettier#autoformat = 1 ]])
-vim.cmd([[ let g:prettier#autoformat_require_pragma = 0 ]])
+vim.cmd [[ let g:prettier#autoformat = 1 ]]
+vim.cmd [[ let g:prettier#autoformat_require_pragma = 0 ]]
 return {
-  "prettier/vim-prettier",
-  config = function ()
-    vim.cmd([[ let g:prettier#autoformat = 1 ]])
-    vim.cmd([[ let g:prettier#autoformat_require_pragma = 0 ]])
+  'prettier/vim-prettier',
+  config = function()
+    vim.cmd [[ let g:prettier#autoformat = 1 ]]
+    vim.cmd [[ let g:prettier#autoformat_require_pragma = 0 ]]
   end,
 }
